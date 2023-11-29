226c6f79c19be39ebb97b0fa54a6bebb3a0f3f74
Shajid <shajid1biz@gmail.com>
2023-11-30 00:02:10 +0530

    array of objects initialized

---
+++ javascript/06-array-objects-loop/main.js
@@ -0,0 +1,15 @@
+// I need to create a book lists in an Array of Objects.
+// Each object should contain Book Name / Book Author / Pages / Genre
+// Logic: I wanted to find How many hours would go to complete each book.
+// ! 238 Words Per Minute (Silent Reader)
+// ! 183 Words Per Minute (Loud Reader)
+// ! Formula -> 2500 Words / 238 Words Per Minute = 10.50 (It Will take approximately ~10 Minutes) (Avergae Silent Reader);
+// ! Formula -> 2500 Words / 183 Words Per Minute = 13.66 (It Will take appriximately ~13 Minutes) (Average Loud Reader);
+
+const bookLists = [
+  { id: 1, bookName: "Think Like A Monk", author: "Jay Shetty", words: 101370 },
+  { id: 2, bookName: "Atomic Habits", author: "James Clear", words: 80000 },
+  { id: 3, bookName: "Courage Is Calling", author: "Ryan Holiday", words: 70000 },
+];
+
+
