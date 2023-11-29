6f38f4ccd2122d0fb6f9b6f05edd84dfea3ae8f6
Shajid <shajid1biz@gmail.com>
2023-11-30 01:18:19 +0530

    converted an array into a number

--- javascript/06-array-objects-loop/main.js
+++ javascript/06-array-objects-loop/main.js
@@ -6,10 +6,49 @@
 // ! Formula -> 2500 Words / 238 Words Per Minute = 10.50 (It Will take approximately ~10 Minutes) (Avergae Silent Reader);
 // ! Formula -> 2500 Words / 183 Words Per Minute = 13.66 (It Will take appriximately ~13 Minutes) (Average Loud Reader);
 
+// First Match with the book, then map and find the correct answer.
+
 const bookLists = [
-  { id: 1, bookName: "Think Like A Monk", author: "Jay Shetty", words: 101370 },
-  { id: 2, bookName: "Atomic Habits", author: "James Clear", words: 80000 },
-  { id: 3, bookName: "Courage Is Calling", author: "Ryan Holiday", words: 70000 },
+  {
+    id: 1,
+    bookName: "Think Like A Monk",
+    author: "Jay Shetty",
+    words: 101370,
+  },
+  {
+    id: 2,
+    bookName: "Atomic Habits",
+    author: "James Clear",
+    words: 80000,
+  },
+  {
+    id: 3,
+    bookName: "Courage Is Calling",
+    author: "Ryan Holiday",
+    words: 70000,
+  },
 ];
 
+const userPrompt = "Courage Is Calling";
+
+const findBook = bookLists.filter(function (value) {
+  return value.bookName === userPrompt;
+}).map(function(value){
+  return parseInt(value.words);
+}) // This will return an array! using parseInt converted the array into an Number. 
+
+// Todo: Source : https://www.tutorialspoint.com/how-to-convert-array-of-strings-to-array-of-numbers-in-javascript
+
+if (findBook) {
+  const totalHours = ((findBook / 238) / 60).toFixed(2)
+  console.log(`
+
+    📕 Book Name: ${findBook.bookName}.
+    ✍🏽 Author: ${findBook.author}.
+    ⌛ Hours to Complete: ${totalHours} Hours.  
+    `
+  );
+} else {
+  console.log("Nothing happened");
+}
 
