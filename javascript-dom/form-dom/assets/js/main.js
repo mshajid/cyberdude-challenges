f832c385800569d789f33d56067ef6730c033f3b
Shajid <shajid1biz@gmail.com>
2023-12-19 00:07:08 +0530

    fix: main.js configs to tailwindcss

--- javascript-dom/form-dom/assets/js/main.js
+++ javascript-dom/form-dom/assets/js/main.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+// If I Click "Submit Now", It should display on the right div. 
+
+// Todo Logic. 
+// Credit Card Number Should be in 16 digits. 
+// CVV Number should be 3 digits. 
+// First Name & Last Name Required. 
+// Primary & Secondary Contact Number Allowed Digits only. 
+// Submit Now button add listen. 
+
+// console.dir(document.querySelector('input[type="radio"]').form[0].name)
+
+const creditRadioBtn = document.querySelector("#creditRadio");
+const creditCardDivEl = document.getElementById("creditCardDiv");
+
+console.log(creditRadioBtn);
+
+
+creditRadioBtn.addEventListener("change", () => {
+    if(creditRadioBtn.checked) {
+        creditCardDivEl.classList.add("bg-[#0075FF]/20","bg-opacity-100")
+    }
+})
+
+// console.log(nameEl.checked);
+
+
+
+// if(nameEl.checked === true) {
+//     creditCardDivEl.classList.add("bg-red-500")
+// } else {
+//     console.log('Not true')
+// }
