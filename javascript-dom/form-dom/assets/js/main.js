3c93c755976e574d2b75dfffae5a0f712ebbde03
Shajid <shajid1biz@gmail.com>
2023-12-20 21:02:21 +0530

    feat: disabled paste options

--- javascript-dom/form-dom/assets/js/main.js
+++ javascript-dom/form-dom/assets/js/main.js
@@ -6,7 +6,8 @@
 // * First Name & Last Name Required.
 // * Primary & Secondary Contact Number Allowed Digits only.
 // * Submit FORM Now button add listen.
-// Hello Name Should Implied With Input Event Type. 
+// * Hello Name Should Implied With Input Event Type.
+// * Cannot Paste Value to Credit Card Number
 
 const creditRadioBtn = document.querySelector("#creditRadio");
 const creditCardDivEl = document.getElementById("creditCardDiv");
@@ -29,13 +30,11 @@ const {
   secondary,
 } = checkOutForm.elements;
 
-
 const allDivEl = document.querySelectorAll("div");
 const fullName = allDivEl[30].children[0];
 const fullAddress = allDivEl[30].children[1];
 const contactNumber = allDivEl[30].children[2];
 const verifiedEmail = allDivEl[30].children[3];
-
 
 checkOutForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -46,49 +45,47 @@ checkOutForm.addEventListener("submit", (event) => {
     console.log("You've been successfully authenitcated");
   }
 
-  if(cvvNumber.value.length < 3 || cvvNumber.value.length > 3){
-    alert("Please Enter the Correct CVV Number")
+  if (cvvNumber.value.length < 3 || cvvNumber.value.length > 3) {
+    alert("Please Enter the Correct CVV Number");
   } else {
-    console.log("You've been successfully authenticated CVV")
+    console.log("You've been successfully authenticated CVV");
   }
 
-  if(firstName.value.length < 2 || firstName.value.length > 15) {
-    console.log("Kindly, Enter Your Proper First Name")
+  if (firstName.value.length < 2 || firstName.value.length > 15) {
+    console.log("Kindly, Enter Your Proper First Name");
   } else {
     fullName.textContent = firstName.value;
     greetNameEl.textContent = firstName.value;
   }
 
-  if(lastName.value.length < 2 || lastName.value.length > 15) {
-    console.log("Kindly, Enter Your Proper Last Name")
+  if (lastName.value.length < 2 || lastName.value.length > 15) {
+    console.log("Kindly, Enter Your Proper Last Name");
   } else {
-    console.log("Nice last name :D")
+    console.log("Nice last name :D");
   }
 
-  if(delivery.value.length < 2 || delivery.value.length > 40) {
-    console.log("Kindly, Enter Your Correct Address To Deliver")
+  if (delivery.value.length < 2 || delivery.value.length > 40) {
+    console.log("Kindly, Enter Your Correct Address To Deliver");
   } else {
-    fullAddress.textContent = delivery.value
+    fullAddress.textContent = delivery.value;
   }
 
-  if(primary.value.length < 10 || primary.value.length > 10) {
+  if (primary.value.length < 10 || primary.value.length > 10) {
     console.log("Enter your correct phone number");
   } else {
-    contactNumber.textContent = primary.value
+    contactNumber.textContent = primary.value;
   }
-  
-  if(email.value === "") {
-    alert("email cannot be empty")
+
+  if (email.value === "") {
+    alert("email cannot be empty");
   } else {
     verifiedEmail.textContent = email.value;
   }
 });
 
-
-
 firstName.addEventListener("input", (event) => {
-  greetNameEl.textContent = firstName.value
-})
+  greetNameEl.textContent = firstName.value;
+});
 
 // To Indicate Which Payment Method You Prefer
 creditRadioBtn.addEventListener("change", (e) => {
@@ -125,6 +122,14 @@ payPalRadioBtn.addEventListener("change", (e) => {
 });
 
 verifyForm.addEventListener("submit", (event) => {
-  event.preventDefault()
-  alert("Thank you")
-})
+  event.preventDefault();
+  alert("Thank you");
+});
+
+const handlePaste = (event) => {
+  event.preventDefault();
+  alert("You cannot paste credit card numbers.")
+}
+
+card.addEventListener("paste", handlePaste);
+cvvNumber.addEventListener("paste", handlePaste);
