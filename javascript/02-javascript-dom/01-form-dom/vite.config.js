fb26faea77de0b7461f72632f38b7bf01534d2e9
Shajid <shajid1biz@gmail.com>
2023-12-29 19:46:47 +0530

    fix: github action and yaml file configured

--- javascript/02-javascript-dom/01-form-dom/vite.config.js
+++ javascript/02-javascript-dom/01-form-dom/vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from "vite";
 
-export default defineConfig({
-    base: "/cyberdude-challenges/",
-})
+// export default defineConfig({
+//     base: "/cyberdude-challenges/",
+// })
 
