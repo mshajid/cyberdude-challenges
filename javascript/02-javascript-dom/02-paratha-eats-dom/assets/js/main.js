60eb5763d01bbdcab1b684936093fbdcd9366205
Shajid <shajid1biz@gmail.com>
2024-01-04 14:01:30 +0530

    feat: gsap init

--- javascript/02-javascript-dom/02-paratha-eats-dom/assets/js/main.js
+++ javascript/02-javascript-dom/02-paratha-eats-dom/assets/js/main.js
@@ -10,6 +10,24 @@ const modalWrapperEl = document.getElementById("modalWrapper");
 const navEl = document.getElementById("aboutUs");
 const modalEl = document.getElementById("modalEl");
 
+import { gsap } from "gsap";
+    
+import { Flip } from "gsap/Flip";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { EaselPlugin } from "gsap/EaselPlugin";
+import { PixiPlugin } from "gsap/PixiPlugin";
+import { TextPlugin } from "gsap/TextPlugin";
+
+gsap.registerPlugin(Flip,ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin,EaselPlugin,PixiPlugin,TextPlugin);
+
+
+gsap.to("#box", { rotation: 180, x: 469, duration: 5 });
+
+
 validate.addField("#fullName", [
   {
     rule: "required",
