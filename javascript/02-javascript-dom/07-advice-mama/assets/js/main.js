2edfc588cd6b6d86137eb203f821aaf075b31fc8
Shajid <shajid1biz@gmail.com>
2024-01-13 18:40:56 +0530

    feat: add a about modal

--- javascript/02-javascript-dom/07-advice-mama/assets/js/main.js
+++ javascript/02-javascript-dom/07-advice-mama/assets/js/main.js
@@ -1,6 +1,13 @@
 const quoteEl = document.getElementById("quote");
 const quoteBtnEl = document.getElementById("quoteBtn");
+const aboutSectionEl = document.getElementById("about");
+const aboutAuthorEl = document.getElementById("aboutAuthor");
 
+const modalSectionEl = document.getElementById("modalSection");
+console.log(modalSectionEl);
+
+console.log(aboutSectionEl)
+console.log(aboutAuthorEl);
 
 const API = "https://api.adviceslip.com/advice";
 
@@ -17,4 +24,15 @@ const fetchAdvice = async () => {
 const fetchData = await fetchAdvice()
 
 quoteEl.textContent = fetchData.slip.advice;
+
+aboutAuthorEl.addEventListener("click", () => {
+    aboutSectionEl.classList.remove("hidden");
+})
+
+modalSectionEl.addEventListener("click", (e) => {
+    if(modalSectionEl === e.target) {
+        aboutSectionEl.classList.add("hidden")
+    }
+})
+
 
