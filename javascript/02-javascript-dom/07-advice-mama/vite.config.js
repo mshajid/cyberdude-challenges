96407a8e200030470eae0816eb0ad8bbee0d1347
Shajid <shajid1biz@gmail.com>
2024-01-13 11:58:21 +0530

    fix: implemented the vite.config.js file to top-level-await true

--- javascript/02-javascript-dom/07-advice-mama/vite.config.js
+++ javascript/02-javascript-dom/07-advice-mama/vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     base: "/cyberdude-challenges/",
+    build: {
+        target: "es2022"
+      },
+      esbuild: {
+        supported: {
+          'top-level-await': true
+        },
+      },
 })
