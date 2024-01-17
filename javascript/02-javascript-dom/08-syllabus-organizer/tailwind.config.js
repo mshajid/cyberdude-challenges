d6eb9f911d1cf4e2830590963c35443c15e70f68
Shajid <shajid1biz@gmail.com>
2024-01-17 12:09:17 +0530

    feat: added hero banner

--- javascript/02-javascript-dom/08-syllabus-organizer/tailwind.config.js
+++ javascript/02-javascript-dom/08-syllabus-organizer/tailwind.config.js
@@ -6,6 +6,11 @@ export default {
       fontFamily: {
         jakarta: "Plus Jakarta Sans",
       },
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
     },
   },
   plugins: [],
