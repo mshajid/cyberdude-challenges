740ef22c55b38fe8a3046f074beac4bb9136fb5e
Shajid <shajid1biz@gmail.com>
2024-01-17 18:47:50 +0530

    feat: added just-validate func

--- javascript/02-javascript-dom/08-syllabus-organizer/assets/js/main.js
+++ javascript/02-javascript-dom/08-syllabus-organizer/assets/js/main.js
@@ -1,18 +1,59 @@
+import JustValidate from "just-validate";
+
+//* These are for Getting Elements for Modal
 const submitVideosEl = document.getElementById("submitVideos");
 const modalWrapperEl = document.getElementById("modalWrapper");
 const sectionWrapperEl = document.getElementById("sectionWrapper");
 const secondWrapperEl = document.getElementById("secondWrapper");
 
+// * Validation Starts Here
 
-// Opens the modal
+const validate = new JustValidate("#yuniversity");
+
+validate.addField("#videoTitle", [
+  {
+    rule: "required",
+  },
+]);
+
+validate.addField("#videoUrl", [
+  {
+    rule: "required",
+  },
+]);
+
+validate.addField("#videoType", [
+  {
+    rule: "required",
+  },
+]);
+
+validate.addField("#channelName", [
+  {
+    rule: "required",
+  },
+]);
+
+validate.addField("#instructor", [
+  {
+    rule: "required",
+  },
+]);
+
+validate.addField("#videoDuration", [{
+    rule: "required",
+}])
+
+// * Validation Ends Here
+
+//* Opens the modal
 submitVideosEl.addEventListener("click", () => {
-    modalWrapperEl.classList.remove("hidden");
-})
+  modalWrapperEl.classList.remove("hidden");
+});
 
-// Closes the modal when clicked outside the body!
-secondWrapperEl.addEventListener("click",(e) => {
-   if(e.target === secondWrapperEl) {
-    modalWrapperEl.classList.add("hidden")
-   }
-})
-
+//* Closes the modal when clicked outside the body!
+secondWrapperEl.addEventListener("click", (e) => {
+  if (e.target === secondWrapperEl) {
+    modalWrapperEl.classList.add("hidden");
+  }
+});
