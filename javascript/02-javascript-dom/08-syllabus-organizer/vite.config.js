df1cd41d95b0c11db5b4a122a10b00af944e01c5
Shajid <shajid1biz@gmail.com>
2024-01-20 03:00:36 +0530

    build: for production

---
+++ javascript/02-javascript-dom/08-syllabus-organizer/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+
+export default defineConfig ({
+    base: "/cyberdude-challenges/",
+    build: {
+        target: "es2022"
+      },
+      esbuild: {
+        supported: {
+          'top-level-await': true
+        },
+      },
+})
+
