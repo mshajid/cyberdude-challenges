7fc736071b925678f8a4f393297a9cd4d314980d
Shajid <shajid1biz@gmail.com>
2024-01-25 17:01:18 +0530

    feat: adding blog card

--- javascript/03-react-js/01-shajid-blog/src/components/BlogCard.jsx
+++ javascript/03-react-js/01-shajid-blog/src/components/BlogCard.jsx
@@ -1,5 +1,16 @@
 const BlogCard = () => {
-  return <div>Blog Card</div>;
+  return (
+    <div>
+      <div className="max-w-3xl bg-white p-5 rounded-lg my-4">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+          vitae provident velit ducimus corrupti eaque aperiam rerum! Nostrum
+          architecto, ad labore, voluptates, nobis cumque debitis porro magnam
+          laudantium maiores corporis.
+        </p>
+      </div>
+    </div>
+  );
 };
 
-export default BlogCard
+export default BlogCard;
