9c9522297d3ac312c452775ef75d9038354db14d
Shajid <shajid1biz@gmail.com>
2024-01-27 14:09:59 +0530

    feat: added the boilerplates to sidebar trending posts

--- javascript/03-react-js/01-shajid-blog/src/components/Trending.jsx
+++ javascript/03-react-js/01-shajid-blog/src/components/Trending.jsx
@@ -1,7 +1,30 @@
+import SidebarPosts from "../data/TrendingPosts";
+
 const TrendingPosts = () => {
+  const trendingPostTemplate = SidebarPosts.map((post) => {
+    return (
+      <div className="bg-[#393E46] px-5 py-2 my-2 text-white rounded-lg hover:bg-gradient-to-r from-blue-800 to-indigo-900 transition-all">
+        <div className="flex gap-x-3">
+          <div>
+            <img src={post.smallFeaturedImg} className="rounded-md" />
+          </div>
+          <div className="flex flex-col gap-y-5">
+            <span className="text-pretty text-sm font-bold">
+              {post.postTitle}
+            </span>
+            <div className="flex items-center gap-x-2">
+              <img src={post.authorImg} className="rounded-md" />
+              <span className="text-[10px]">By. {post.authorName}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
   return (
-    <div className="bg-[#393E46] px-5 py-7 text-white rounded-lg hover:bg-gradient-to-r from-blue-800 to-indigo-900 transition-all">
-      <span>Click Me</span>
+    <div>
+      <div className="text-2xl font-semibold">Trending Posts</div>
+      {trendingPostTemplate}
     </div>
   );
 };
---
+++ javascript/03-react-js/01-shajid-blog/src/data/TrendingPosts.jsx
@@ -0,0 +1,40 @@
+const SideBarPosts = [
+  {
+    postID: 1,
+    category: "learning-tips",
+    smallFeaturedImg: "https://placehold.co/100x100",
+    authorImg: "https://placehold.co/20x20",
+    authorName: "Muhammed Shajid Shafee",
+    datePublished: "January 01, 2024",
+    postTitle: "How To Become A Web Developer",
+  },
+  {
+    postID: 1,
+    category: "learning-tips",
+    smallFeaturedImg: "https://placehold.co/100x100",
+    authorImg: "https://placehold.co/20x20",
+    authorName: "Muhammed Shajid Shafee",
+    datePublished: "January 01, 2024",
+    postTitle: "How To Become A Web Developer",
+  },
+  {
+    postID: 1,
+    category: "learning-tips",
+    smallFeaturedImg: "https://placehold.co/100x100",
+    authorImg: "https://placehold.co/20x20",
+    authorName: "Muhammed Shajid Shafee",
+    datePublished: "January 01, 2024",
+    postTitle: "How To Become A Web Developer",
+  },
+  {
+    postID: 1,
+    category: "learning-tips",
+    smallFeaturedImg: "https://placehold.co/100x100",
+    authorImg: "https://placehold.co/20x20",
+    authorName: "Muhammed Shajid Shafee",
+    datePublished: "January 01, 2024",
+    postTitle: "How To Become A Web Developer",
+  },
+];
+
+export default SideBarPosts;
