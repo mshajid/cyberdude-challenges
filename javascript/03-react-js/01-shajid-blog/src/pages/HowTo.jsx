5fbf82d5d5f53150643087ae5e76578bf5ca8e4e
Shajid <shajid1biz@gmail.com>
2024-01-27 17:38:10 +0530

    feat: added how-to post

--- javascript/03-react-js/01-shajid-blog/src/pages/HowTo.jsx
+++ javascript/03-react-js/01-shajid-blog/src/pages/HowTo.jsx
@@ -1,7 +1,14 @@
 const HowTo = () => {
-    return(
-        <div>This is How to</div>
-    )
-}
+  return (
+    <div className="max-w-4xl bg-white p-5 rounded-lg my-4">
+      <h2 className="text-3xl font-semibold mb-4">How To Become A Web Developer In 6 Months</h2>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident quis
+        eaque et sapiente quo magnam animi recusandae, accusamus atque excepturi
+        tenetur tempore veniam est dolor, odio earum. Quibusdam, quae veniam!
+      </div>
+    </div>
+  );
+};
 
-export default HowTo
+export default HowTo;
