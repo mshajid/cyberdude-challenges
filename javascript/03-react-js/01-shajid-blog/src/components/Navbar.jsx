971cb6270c4819780ec2f0b46c3dd470cb67cc0c
Shajid <shajid1biz@gmail.com>
2024-01-28 00:00:47 +0530

    feat: header responsive

--- javascript/03-react-js/01-shajid-blog/src/components/Navbar.jsx
+++ javascript/03-react-js/01-shajid-blog/src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="max-w-6xl mx-auto bg-[#222831] py-2 rounded-lg shadow-lg shadow-black/10">
-      <header className="flex items-center justify-between px-10 py-2">
+      <header className="text-center justify-center sm:flex sm:items-center sm:justify-between px-10 py-2">
         <Link to={"/"}>
           <h2 className="text-white text-3xl">Shajid</h2>
         </Link>
-        <ul className="flex gap-x-4 text-[#D9D9D9] text-lg">
+        <ul className="flex justify-center mt-2 sm:flex gap-x-4 text-[#D9D9D9] text-lg">
           <li
             className="hover:text-white cursor-pointer text-[15px]"
             title="go to home"
@@ -29,7 +29,7 @@ const Navbar = () => {
         </ul>
       </header>
       <div className="h-[2px] mx-10 my-5 bg-white w-fullrounded-lg"></div>
-      <div className="px-10 text-white text-3xl font-bold tracking-[0.4em] mb-4">
+      <div className="sm:px-10 text-center sm:text-left text-white sm:text-3xl font-bold tracking-[0.4em] mb-4">
         From HTML to REACT 🚀
       </div>
     </div>
