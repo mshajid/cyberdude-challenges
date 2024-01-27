1feebf1c989908e004a78cbd05f09ff45411fcc9
Shajid <shajid1biz@gmail.com>
2024-01-27 20:33:13 +0530

    feat: title leads to the post

--- javascript/03-react-js/01-shajid-blog/src/components/BlogCard.jsx
+++ javascript/03-react-js/01-shajid-blog/src/components/BlogCard.jsx
@@ -2,17 +2,16 @@ import { Link } from "react-router-dom";
 import BlogData from "../data/Posts";
 
 const BlogCard = () => {
-
   const postTemplate = BlogData.map((post) => {
     return (
       <div key={post.postID} className="max-w-4xl bg-white p-5 rounded-lg my-4">
-        <h1 className="text-3xl font-bold">{post.postTitle}</h1>
+        <h1 className="text-3xl font-bold">
+          <Link to={`coding-journey/${post.postLink}`}>{post.postTitle}</Link>
+        </h1>
         <div className="flex gap-x-3 items-center my-2">
           <div className="flex-1 flex gap-x-3 items-center">
             <span className="text-sm font-medium">{post.author}</span>
-            <span
-              className="text-sm font-bold bg-[#FFD369] px-3 text-black rounded-md"
-            >
+            <span className="text-sm font-bold bg-[#FFD369] px-3 text-black rounded-md">
               {post.readTime}
             </span>
           </div>
@@ -24,7 +23,12 @@ const BlogCard = () => {
         <img src={post.featuredImg} className="rounded my-4 w-full" />
         <p className="my-4">{post.postDesc}</p>
         <button className="my-3">
-          <Link className="bg-[#FFD369] text-black hover:bg-[#222831] hover:text-white px-5 py-1.5 rounded-md transition-all" to={`/coding-journey/${post.postLink}`}>Read more here</Link>
+          <Link
+            className="bg-[#FFD369] text-black hover:bg-[#222831] hover:text-white px-5 py-1.5 rounded-md transition-all"
+            to={`/coding-journey/${post.postLink}`}
+          >
+            Read more here
+          </Link>
         </button>
       </div>
     );
