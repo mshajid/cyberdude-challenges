127818fdbfadb3ec11092453a33f1e2846420079
Shajid <shajid1biz@gmail.com>
2024-01-27 13:00:11 +0530

    feat: added author information on featured posts

--- javascript/03-react-js/01-shajid-blog/src/components/BlogCard.jsx
+++ javascript/03-react-js/01-shajid-blog/src/components/BlogCard.jsx
@@ -1,17 +1,25 @@
 import { Link, useParams } from "react-router-dom";
 import BlogData from "../data/Posts";
 
-
-
 const BlogCard = () => {
-  const params = useParams()
+  const params = useParams();
   console.log(params);
 
   const postTemplate = BlogData.map((post) => {
     return (
       <div className="max-w-4xl bg-white p-5 rounded-lg my-4">
-        <h1 className="text-3xl font-semibold">{post.postTitle}</h1>
-        <img src={post.featuredImg}/>
+        <h1 className="text-3xl font-bold">{post.postTitle}</h1>
+        <div className="flex gap-x-3 items-center my-2">
+          <div className="flex-1 flex gap-x-3 items-center">
+            <span className="text-sm font-medium">{post.author}</span>
+            <span className="text-sm font-bold bg-[#FFD369] px-3 text-black rounded-md">{post.readTime}</span>
+          </div>
+          <div className="flex gap-x-2">
+            <span className="text-sm font-regular">Published:</span>
+            <span className="text-sm font-bold">{post.datePublished}</span>
+          </div>
+        </div>
+        <img src={post.featuredImg} className="rounded my-4" />
         <p>{post.postDesc}</p>
         <button>
           <Link to={`/coding-journey/${post.postLink}`}>Click Here</Link>
--- javascript/03-react-js/01-shajid-blog/src/data/Posts.jsx
+++ javascript/03-react-js/01-shajid-blog/src/data/Posts.jsx
@@ -3,7 +3,10 @@ const BlogData = [
     postID: 1,
     category: "coding-journey",
     postLink: "how-to-be-a-dev",
-    featuredImg:"https://placehold.co/600x400",  
+    featuredImg:"https://placehold.co/800x400",
+    author: "Muhammed Shajid Shafee",
+    readTime: "~2 mins",
+    datePublished: "January 01, 2024",
     postTitle: "How To Become A Web Developer",
     postDesc:
       "lorem ipsum akdad asd kasjdk jaskdj kasjdlj asjdk jasjd klajlsk djlkajsdkl jlaksj dkljaslk dlakjsd asd asdkjas jdkljas kldjas ",
@@ -12,6 +15,7 @@ const BlogData = [
     postID: 2,
     category: "coding-journey",
     postLink: "how-to-be-a-dev",
+    featuredImg:"https://placehold.co/800x400", 
     postTitle: "10 Visual Studio Code Secrets",
     postDesc:
       "lorem ipsum akdad asd kasjdk jaskdj kasjdlj asjdk jasjd klajlsk djlkajsdkl jlaksj dkljaslk dlakjsd asd asdkjas jdkljas kldjas ",
