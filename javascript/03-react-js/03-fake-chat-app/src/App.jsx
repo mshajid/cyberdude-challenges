b445fe360c6aa459cf914b29bb9014d97502dc35
Shajid <shajid1biz@gmail.com>
2024-02-01 02:50:09 +0530

    project initialized

---
+++ javascript/03-react-js/03-fake-chat-app/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="font-jakarta">
+      <span className="bg-teal-500 px-5 py-10 text-2xl font-semibold">Hello React</span>
+    </div>
+  );
+};
+
+export default App;
