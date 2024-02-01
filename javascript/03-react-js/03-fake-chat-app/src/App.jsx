7e265c7e254306ad72ee9806a17da5e6814a7e15
Shajid <shajid1biz@gmail.com>
2024-02-01 16:42:36 +0530

    fix: min-h-screen for the app

--- javascript/03-react-js/03-fake-chat-app/src/App.jsx
+++ javascript/03-react-js/03-fake-chat-app/src/App.jsx
@@ -2,7 +2,7 @@ import ChatApp from "./Components/ChatApp";
 
 const App = () => {
   return (
-    <div className="font-jakarta">
+    <div className="font-jakarta bg-[#dcf8c6] min-h-screen">
       <ChatApp />
     </div>
   );
