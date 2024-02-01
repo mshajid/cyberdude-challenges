6b853c54d99d3ea440e4d31b3f9274161ae40851
Shajid <shajid1biz@gmail.com>
2024-02-01 13:10:06 +0530

    fix: changes made in the templat

--- javascript/03-react-js/04-blog-with-api/src/Components/PostTemplate.jsx
+++ javascript/03-react-js/04-blog-with-api/src/Components/PostTemplate.jsx
@@ -15,13 +15,13 @@ const PostTemplate = () => {
   const blogTemplate = post.map((eachPost) => {
     return (
       <div key={eachPost.id} className="max-w-xs">
-        <div className="flex flex-col gap-y-2">
+        <div className="flex flex-col gap-y-2 overflow-hidden">
           <img className="rounded-md" src="https://placekitten.com/200/200" />
-          <h2 className="text-2xl font-semibold mb-2">{eachPost.title}</h2>
-          <p className="max-w-xs text-pretty">{eachPost.body}</p>
-          <div className="flex gap-x-2 items-end h-12">
+          <h2 className="text-xl font-semibold mb-2 h-12">{eachPost.title}</h2>
+          <p className="max-w-xs text-pretty h-12 overflow-y-clip">{eachPost.body}</p>
+          <div className="flex gap-x-2 items-end">
             <div className="text-white flex gap-x-2 items-center">
-              <span className="text-black">Related Tags</span>
+              <span className="text-black font-semibold">Related Tags</span>
               {eachPost.tags.map((tag, index) => {
                 return (
                   <span key={`tag-${index}`} className="px-2 bg-[#735751] rounded-md">{tag}</span>
@@ -29,7 +29,7 @@ const PostTemplate = () => {
               })}
             </div>
           </div>
-          <Link to={`/blog/${eachPost.id}`}>Read Here</Link>
+          <Link className="bg-[#735751] py-0.5 text-center my-5 rounded-md text-sm text-white" to={`/blog/${eachPost.id}`}>Read Here</Link>
         </div>
       </div>
     );
