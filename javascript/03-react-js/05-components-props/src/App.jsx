d7f6c812d9123bd4d1fcdf9bdb9aaa22a56ae79f
Shajid <shajid1biz@gmail.com>
2024-02-06 12:48:35 +0530

    chore: added a dummy content to fill the gap

--- javascript/03-react-js/05-components-props/src/App.jsx
+++ javascript/03-react-js/05-components-props/src/App.jsx
@@ -2,8 +2,9 @@ import Button from "./Components/Button";
 
 const App = () => {
   return (
-    <div className="px-5 flex flex-col gap-x-2">
-      <h2 className="font-semibold text-3xl">Buttons</h2>
+    <div className="px-5 flex flex-col gap-y-2">
+      <h2 className="font-semibold text-4xl">Buttons</h2>
+      <span className="text-lg">Check all the button variants here with all the live examples</span>
       <div className="flex gap-x-5">
         <Button variant="Primary" />
         <Button variant="Secondary" />
--- javascript/03-react-js/05-components-props/src/main.jsx
+++ javascript/03-react-js/05-components-props/src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <div className="font-Jakarta">
+  <div className="font-Jakarta py-10">
     <App />
   </div>
 );
