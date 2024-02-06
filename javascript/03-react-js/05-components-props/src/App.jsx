e492833c579d31155fca4955f374285fbb06db73
Shajid <shajid1biz@gmail.com>
2024-02-06 12:44:24 +0530

    fix: changed the text-size of the each btn

--- javascript/03-react-js/05-components-props/src/App.jsx
+++ javascript/03-react-js/05-components-props/src/App.jsx
@@ -1,18 +1,21 @@
 import Button from "./Components/Button";
 
-const App =() => {
+const App = () => {
   return (
-    <div className="px-5 flex gap-x-2">
-      <Button variant="Primary" />
-      <Button variant="Secondary" />
-      <Button variant="Success" />
-      <Button variant="Warning" />
-      <Button variant="Danger" />
-      <Button variant="Info" />
-      <Button variant="Light" />
-      <Button variant="Dark" />
+    <div className="px-5 flex flex-col gap-x-2">
+      <h2 className="font-semibold text-3xl">Buttons</h2>
+      <div className="flex gap-x-5">
+        <Button variant="Primary" />
+        <Button variant="Secondary" />
+        <Button variant="Success" />
+        <Button variant="Warning" />
+        <Button variant="Danger" />
+        <Button variant="Info" />
+        <Button variant="Light" />
+        <Button variant="Dark" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- javascript/03-react-js/05-components-props/src/Components/Button.jsx
+++ javascript/03-react-js/05-components-props/src/Components/Button.jsx
@@ -3,19 +3,19 @@ import React from "react";
 const Button = ({ variant }) => {
   const variants = {
     Primary:
-      "bg-indigo-500/40 text-indigo-900 px-2 py-1 rounded-md border border-indigo-500 hover:bg-indigo-500 hover:text-white transition-all",
+      "bg-indigo-500/40 text-indigo-900 px-2 py-1 rounded-md border border-indigo-500 hover:bg-indigo-500 hover:text-white transition-all text-sm",
     Secondary:
-      "bg-zinc-500/40 text-zinc-900 px-2 py-1 rounded-md border border-zinc-500 hover:bg-zinc-500 hover:text-white transition-all",
+      "bg-zinc-500/40 text-zinc-900 px-2 py-1 rounded-md border border-zinc-500 hover:bg-zinc-500 hover:text-white transition-all text-sm",
     Success:
-      "bg-emerald-500/40 text-emerald-900 px-2 py-1 rounded-md border border-emerald-500 hover:bg-emerald-500 hover:text-white transition-all",
+      "bg-emerald-500/40 text-emerald-900 px-2 py-1 rounded-md border border-emerald-500 hover:bg-emerald-500 hover:text-white transition-all text-sm",
     Warning:
-      "bg-amber-500/40 text-amber-900 px-2 py-1 rounded-md border border-amber-500 hover:bg-amber-500 hover:text-white transition-all",
+      "bg-amber-500/40 text-amber-900 px-2 py-1 rounded-md border border-amber-500 hover:bg-amber-500 hover:text-white transition-all text-sm",
     Danger:
-      "bg-rose-500/40 text-rose-900 px-2 py-1 rounded-md border border-rose-500 hover:bg-rose-500 hover:text-white transition-all",
-    Info: "bg-sky-500/40 text-sky-900 px-2 py-1 rounded-md border border-sky-500 hover:bg-sky-500 hover:text-white transition-all",
+      "bg-rose-500/40 text-rose-900 px-2 py-1 rounded-md border border-rose-500 hover:bg-rose-500 hover:text-white transition-all text-sm",
+    Info: "bg-sky-500/40 text-sky-900 px-2 py-1 rounded-md border border-sky-500 hover:bg-sky-500 hover:text-white transition-all text-sm",
     Light:
-      "bg-white text-slate-900 px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-200 hover:text-black transition-all",
-    Dark: "bg-slate-950/60 text-slate-950 px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-900 hover:text-white transition-all",
+      "bg-white text-slate-900 px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-200 hover:text-black transition-all text-sm",
+    Dark: "bg-slate-950/60 text-slate-950 px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-900 hover:text-white transition-all text-sm",
   };
   return (
     <>
