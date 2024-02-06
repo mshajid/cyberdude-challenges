2b2c7a362db4b8d2d6a972d39573365f19dfb9d2
Shajid <shajid1biz@gmail.com>
2024-02-06 17:23:18 +0530

    feat: added alerts component

--- javascript/03-react-js/05-components-props/src/App.jsx
+++ javascript/03-react-js/05-components-props/src/App.jsx
@@ -2,15 +2,16 @@ import Button from "./Components/Button";
 import { BiDollar } from "react-icons/bi";
 import { BiDownload } from "react-icons/bi";
 import { FaCheck } from "react-icons/fa";
-
-
+import Alert from "./Components/Alert";
 
 const App = () => {
   return (
     <div className="px-5 flex flex-col gap-y-4">
       <div>
-      <h2 className="font-bold text-4xl">Buttons</h2>
-      <span className="text-lg">Check all the button variants here with all the live examples</span>
+        <h2 className="font-bold text-4xl">Buttons</h2>
+        <span className="text-lg">
+          Check all the button variants here with all the live examples
+        </span>
       </div>
       <div className="flex gap-x-5">
         <Button variant="Primary" />
@@ -20,21 +21,78 @@ const App = () => {
         <Button variant="Danger" />
         <Button variant="Info" />
         <Button variant="Light" />
-        <Button variant="Dark" />             
+        <Button variant="Dark" />
       </div>
       <h2 className="font-semibold text-2xl">Buttons With Icons</h2>
       <div className="flex gap-x-5">
-        <Button className={"flex shadow-md items-center gap-x-2"} variant="Primary">
+        <Button
+          className={"flex shadow-md items-center gap-x-2"}
+          variant="Primary"
+        >
           <BiDollar />
         </Button>
-        <Button className={"flex shadow-md items-center gap-x-2"} variant="Light">
+        <Button
+          className={"flex shadow-md items-center gap-x-2"}
+          variant="Light"
+        >
           <BiDownload />
         </Button>
-        <Button className={"flex shadow-md items-center gap-x-2"} variant="Success">
+        <Button
+          className={"flex shadow-md items-center gap-x-2"}
+          variant="Success"
+        >
           <FaCheck />
         </Button>
       </div>
-      <hr className="my-10"/>
+      <hr className="my-5" />
+      <div>
+        <h2 className="font-bold text-4xl">Alerts</h2>
+        <span className="text-lg">Check all the Alerts variants here with all the live examples, Included, With Link & Icons also without Links & Icons</span>
+      </div>
+      <div className="space-y-2">
+        <Alert
+          className={"max-w-xl py-2 text-sm px-5 font-semibold flex gap-x-1"}
+          variant={"Primary"}
+          label={`This is Primary Alert - `}
+          link = {"With Link"}
+          linkTo = {"https://google.com"}
+        />
+        <Alert
+          className={"max-w-xl py-2 text-sm px-5 font-semibold flex gap-x-1"}
+          variant={"Secondary"}
+          label={`This is Secondary Alert - `}
+          link = {"With Link"}
+          linkTo = {"https://google.com"}
+        />
+        <Alert
+          className={"max-w-xl py-2 text-sm px-5 font-semibold flex gap-x-1"}
+          variant={"Success"}
+          label={`This is Success Alert - `}
+          link = {"With Link"}
+          linkTo = {"https://google.com"}
+        />
+        <Alert
+          className={"max-w-xl py-2 text-sm px-5 font-semibold flex gap-x-1"}
+          variant={"Warning"}
+          label={`This is Warning Alert - `}
+          link = {"With Link"}
+          linkTo = {"https://google.com"}
+        />
+        <Alert
+          className={"max-w-xl py-2 text-sm px-5 font-semibold flex gap-x-1"}
+          variant={"Danger"}
+          label={`This is Danger Alert - `}
+          link = {"With Link"}
+          linkTo = {"https://google.com"}
+        />
+        <Alert
+          className={"max-w-xl py-2 text-sm px-5 font-semibold flex gap-x-1"}
+          variant={"Info"}
+          label={`This is Info Alert - `}
+          link = {"With Link"}
+          linkTo = {"https://google.com"}
+        />
+      </div>
     </div>
   );
 };
---
+++ javascript/03-react-js/05-components-props/src/Components/Alert.jsx
@@ -0,0 +1,32 @@
+const Alert = ({ variant, label, className, link, linkTo }) => {
+  const variants = {
+    Primary:
+      "bg-indigo-500/70 text-indigo-900 border-2 border-indigo-500 rounded text-indigo-950",
+    Secondary:
+      "bg-zinc-500/70 text-zinc-900 border-2 border-zinc-500 rounded text-zinc-950",
+    Success:
+      "bg-emerald-500/70 text-emerald-900 border-2 border-emerald-500 rounded text-emerald-950",
+    Warning:
+      "bg-amber-500/70 text-amber-900 border-2 border-amber-500 rounded text-amber-950",
+    Danger:
+      "bg-rose-500/70 text-rose-900 border-2 border-rose-500 rounded text-rose-950",
+    Info:
+      "bg-sky-500/70 text-sky-900 border-2 border-sky-500 rounded text-sky-950",
+  };
+  return (
+    <>
+      <div className={`${variants[variant]} ${className}`}>
+        {label}
+        <a
+          className="hover:text-white transition-all"
+          target="_blank"
+          href={linkTo}
+        >
+          {link}
+        </a>
+      </div>
+    </>
+  );
+};
+
+export default Alert;
