e7d3168819bc3a4178d2c1f41196fe59c40a056f
Shajid <shajid1biz@gmail.com>
2024-02-06 12:35:56 +0530

    feat: added the color variants to the Button component

--- javascript/03-react-js/05-components-props/src/App.jsx
+++ javascript/03-react-js/05-components-props/src/App.jsx
@@ -2,8 +2,15 @@ import Button from "./Components/Button";
 
 const App =() => {
   return (
-    <div className="px-5">
+    <div className="px-5 flex gap-x-2">
       <Button variant="Primary" />
+      <Button variant="Secondary" />
+      <Button variant="Success" />
+      <Button variant="Warning" />
+      <Button variant="Danger" />
+      <Button variant="Info" />
+      <Button variant="Light" />
+      <Button variant="Dark" />
     </div>
   )
 }
--- javascript/03-react-js/05-components-props/src/Components/Button.jsx
+++ javascript/03-react-js/05-components-props/src/Components/Button.jsx
@@ -3,14 +3,19 @@ import React from "react";
 const Button = ({ variant }) => {
   const variants = {
     Primary:
-      "bg-violet-400/20 text-violet-900 px-2 py-1 rounded-md border border-violet-500 hover:bg-violet-400 hover:text-white transition-all",
-    Secondary: "bg-red-500 text-white px-2 py-1 rounded-md",
-    Success: "bg-red-500 text-white px-2 py-1 rounded-md",
-    Warning: "bg-red-500 text-white px-2 py-1 rounded-md",
-    Danger: "bg-red-500 text-white px-2 py-1 rounded-md",
-    Info: "bg-red-500 text-white px-2 py-1 rounded-md",
-    Light: "bg-red-500 text-white px-2 py-1 rounded-md",
-    Dark: "bg-red-500 text-white px-2 py-1 rounded-md",
+      "bg-indigo-500/40 text-indigo-900 px-2 py-1 rounded-md border border-indigo-500 hover:bg-indigo-500 hover:text-white transition-all",
+    Secondary:
+      "bg-zinc-500/40 text-zinc-900 px-2 py-1 rounded-md border border-zinc-500 hover:bg-zinc-500 hover:text-white transition-all",
+    Success:
+      "bg-emerald-500/40 text-emerald-900 px-2 py-1 rounded-md border border-emerald-500 hover:bg-emerald-500 hover:text-white transition-all",
+    Warning:
+      "bg-amber-500/40 text-amber-900 px-2 py-1 rounded-md border border-amber-500 hover:bg-amber-500 hover:text-white transition-all",
+    Danger:
+      "bg-rose-500/40 text-rose-900 px-2 py-1 rounded-md border border-rose-500 hover:bg-rose-500 hover:text-white transition-all",
+    Info: "bg-sky-500/40 text-sky-900 px-2 py-1 rounded-md border border-sky-500 hover:bg-sky-500 hover:text-white transition-all",
+    Light:
+      "bg-white text-slate-900 px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-200 hover:text-black transition-all",
+    Dark: "bg-slate-950/60 text-slate-950 px-2 py-1 rounded-md border border-slate-500 hover:bg-slate-900 hover:text-white transition-all",
   };
   return (
     <>
