105bab91cf83915e7d6ccdd4f8b96d7be63f5125
Shajid <shajid1biz@gmail.com>
2024-02-06 12:20:24 +0530

    feat: added the primary color configs

--- javascript/03-react-js/05-components-props/src/App.jsx
+++ javascript/03-react-js/05-components-props/src/App.jsx
@@ -3,7 +3,7 @@ import Button from "./Components/Button";
 const App =() => {
   return (
     <div className="px-5">
-      <Button variant="Secondary" />
+      <Button variant="Primary" />
     </div>
   )
 }
--- javascript/03-react-js/05-components-props/src/Components/Button.jsx
+++ javascript/03-react-js/05-components-props/src/Components/Button.jsx
@@ -2,14 +2,15 @@ import React from "react";
 
 const Button = ({ variant }) => {
   const variants = {
-    Primary: "bg-violet-500 text-white px-2 py-1",
-    Secondary: "bg-red-500 text-white px-2 py-1",
-    Success: "bg-red-500 text-white px-2",
-    Warning: "bg-red-500 text-white px-2",
-    Danger: "bg-red-500 text-white px-2",
-    Info: "bg-red-500 text-white px-2",
-    Light: "bg-red-500 text-white px-2",
-    Dark: "bg-red-500 text-white px-2",
+    Primary:
+      "bg-violet-400/20 text-violet-900 px-2 py-1 rounded-md border border-violet-500 hover:bg-violet-400 hover:text-white transition-all",
+    Secondary: "bg-red-500 text-white px-2 py-1 rounded-md",
+    Success: "bg-red-500 text-white px-2 py-1 rounded-md",
+    Warning: "bg-red-500 text-white px-2 py-1 rounded-md",
+    Danger: "bg-red-500 text-white px-2 py-1 rounded-md",
+    Info: "bg-red-500 text-white px-2 py-1 rounded-md",
+    Light: "bg-red-500 text-white px-2 py-1 rounded-md",
+    Dark: "bg-red-500 text-white px-2 py-1 rounded-md",
   };
   return (
     <>
