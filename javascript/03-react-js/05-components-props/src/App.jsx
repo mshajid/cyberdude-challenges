94e664db25104d2006b2665d7d7232b0149788c2
Shajid <shajid1biz@gmail.com>
2024-02-07 10:21:38 +0530

    feat: wrapped the headers in comp

--- javascript/03-react-js/05-components-props/src/App.jsx
+++ javascript/03-react-js/05-components-props/src/App.jsx
@@ -7,16 +7,12 @@ import Cards from "./Components/Cards";
 import CardImage from "./Components/CardsImage";
 import CardBody from "./Components/CardsBody";
 import CardsButton from "./Components/CardsButton";
+import ButtonHeader from "./Layouts/ButtonHeader";
 
 const App = () => {
   return (
     <div className="px-5 flex flex-col gap-y-4">
-      <div>
-        <h2 className="font-bold text-4xl">Buttons</h2>
-        <span className="text-lg">
-          Check all the button variants here with all the live examples
-        </span>
-      </div>
+      <ButtonHeader />
       <div className="flex gap-x-5">
         <Button variant="Primary" />
         <Button variant="Secondary" />
---
+++ javascript/03-react-js/05-components-props/src/Layouts/ButtonHeader.jsx
@@ -0,0 +1,15 @@
+const ButtonHeader = () => {
+  return (
+    <div className="max-w-xl">
+      <h2 className="font-bold text-4xl">Buttons</h2>
+      <span className="text-xl font-light my-2 text-gray-500">
+        Use our custom button styles for any action forms, and UIs. All you've to simply put what variant you need.
+      </span>
+      <div className="text-lg my-2">
+        <span>We help you to build your React UI components such as Buttons much faster than you think. What you've to do is invoke the Button component and pass down the variant which one you actually needed <span className="font-semibold">"Primary"</span></span>
+      </div>
+    </div>
+  );
+};
+
+export default ButtonHeader;
