c0b8626ccc43c00062b88eec58d94c37ad248908
Shajid <shajid1biz@gmail.com>
2024-02-08 12:55:32 +0530

    feat: added the onSubmit func

--- javascript/03-react-js/06-react-form/src/Components/Form.jsx
+++ javascript/03-react-js/06-react-form/src/Components/Form.jsx
@@ -6,31 +6,38 @@ const Form = () => {
   const [form, setForm] = useState({ firstName: "", lastName: "" });
   const handleInputs = (e) => {
     const { name, value } = e.target;
-    setForm({ ...form, [name]:value });
+    setForm({ ...form, [name]: value });
     console.log(form);
   };
+
+  const submitForm = (e) => {
+    e.preventDefault()
+    console.log("submitted");
+  }
   return (
-    <div className="flex flex-col gap-y-2">
-      <FormInputs
-        name={"firstName"}
-        id={"firstName"}
-        type={"text"}
-        placeholder={"Enter your first name here"}
-        value={form.firstName}
-        label={"First Name"}
-        handleOnChange={handleInputs}
-      />
-      <FormInputs
-        name={"lastName"}
-        id={"lastName"}
-        type={"text"}
-        placeholder={"Enter your last name here"}
-        value={form.lastName}
-        label={"Last Name"}
-        handleOnChange={handleInputs}
-      />
-      <FormButton label={"Verify"}/>
-    </div>
+    <form onSubmit={submitForm}>
+      <div className="flex flex-col gap-y-2">
+        <FormInputs
+          name={"firstName"}
+          id={"firstName"}
+          type={"text"}
+          placeholder={"Enter your first name here"}
+          value={form.firstName}
+          label={"First Name"}
+          handleOnChange={handleInputs}
+        />
+        <FormInputs
+          name={"lastName"}
+          id={"lastName"}
+          type={"text"}
+          placeholder={"Enter your last name here"}
+          value={form.lastName}
+          label={"Last Name"}
+          handleOnChange={handleInputs}
+        />
+        <FormButton label={"Verify"} />
+      </div>
+    </form>
   );
 };
 
