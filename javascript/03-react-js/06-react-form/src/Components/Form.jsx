79ac8e1467adc9dce2f104a02a83d9c551707be7
Shajid <shajid1biz@gmail.com>
2024-02-13 14:30:45 +0530

    feat: passed the props to component

--- javascript/03-react-js/06-react-form/src/Components/Form.jsx
+++ javascript/03-react-js/06-react-form/src/Components/Form.jsx
@@ -14,8 +14,24 @@ const Form = () => {
   return (
     <>
       <form className="flex flex-col gap-y-2" onSubmit={handleSubmit(onSubmit)}>
-        <input
-          className={`border px-2 text-sm py-1.5 rounded outline-none ${errors.fullName ? 'border border-red-500' : 'border'}`}
+        <FormInputs
+          name="fullName"
+          label="Full Name"
+          placeholder="Enter Your Full Name Here"
+          register={register("fullName", {
+            required: "Olunga name ah kududa vengi",
+            minLength: {
+              value: 3,
+              message: "Requires more than 3 characters",
+            },
+          })}
+          error={errors.fullName}
+        />
+
+        {/* <input
+          className={`border px-2 text-sm py-1.5 rounded outline-none ${
+            errors.fullName ? "border border-red-500" : "border"
+          }`}
           name="fullName"
           placeholder="Enter your name"
           type="text"
@@ -31,10 +47,10 @@ const Form = () => {
           <small className="text-red-500 text-[10px] mb-2 -mt-1.5">
             {errors && errors.fullName.message}
           </small>
-        )}
-        <button className="bg-red-500 rounded px-2 py-0.5  text-white">
+        )} */}
+        {/* <button className="bg-red-500 rounded px-2 py-0.5  text-white">
           Submit
-        </button>
+        </button> */}
       </form>
     </>
   );
--- javascript/03-react-js/06-react-form/src/Components/FormInputs.jsx
+++ javascript/03-react-js/06-react-form/src/Components/FormInputs.jsx
@@ -1,4 +1,4 @@
-const FormInputs = ({ id, type, placeholder, label, register }) => {
+const FormInputs = ({ id, type = "text", placeholder, label, register, error }) => {
   return (
     <div className="flex flex-col gap-y-1">
       <label className="font-semibold tracking-wider" htmlFor={id}>
@@ -9,9 +9,14 @@ const FormInputs = ({ id, type, placeholder, label, register }) => {
         id={id}
         type={type}
         placeholder={placeholder}
+        {...register}
         className="rounded-sm outline-none px-2 py-1.5 w-72 bg-gray-500 text-white text-sm"
-        register = {register}
       />
+      {error && (
+        <small className="text-red-500 text-[10px] mb-2 -mt-1.5">
+          {error.message}
+        </small>
+      )}
     </div>
   );
 };
