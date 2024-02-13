8bcdcde97f55e09c4081a65859298b40e6e65ae0
Shajid <shajid1biz@gmail.com>
2024-02-13 14:44:33 +0530

    feat: added navbar to the app

--- javascript/03-react-js/05-components-props/src/Components/Navbar.jsx
+++ javascript/03-react-js/05-components-props/src/Components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar =()=> {
     return (
         <div className="bg-violet-500 py-4 px-5 flex items-center justify-between mb-10">
             <div className="flex flex-col gap-x-2">
-                <h2 className="text-3xl text-violet-950 font-black">Kai-UI Block</h2>
+                <h2 className="text-3xl text-violet-950 font-black">Shajid UI Block</h2>
                 <span className="text-sm text-white">Simple UI component that make you faster development</span>
             </div>
             <div>
