971bdaacd9b975e6532b5783027074c129fa8bf3
Shajid <shajid1biz@gmail.com>
2024-02-15 15:47:16 +0530

    chore: removed the strict mode due to rendering 2 times

--- javascript/03-react-js/07-my-bucket-lists/src/main.jsx
+++ javascript/03-react-js/07-my-bucket-lists/src/main.jsx
@@ -4,9 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <div className="h-screen bg-[#424B54]">
-      <App />
-    </div>
-  </React.StrictMode>
+  <div className="h-screen bg-[#424B54]">
+    <App />
+  </div>
 );
