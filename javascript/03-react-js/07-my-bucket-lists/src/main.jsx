33be9182a5b1469da42767fe1aa41378bff4f10a
Shajid <shajid1biz@gmail.com>
2024-02-15 15:20:27 +0530

    fix: changed the layout

--- javascript/03-react-js/07-my-bucket-lists/src/main.jsx
+++ javascript/03-react-js/07-my-bucket-lists/src/main.jsx
@@ -5,8 +5,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
+    <div className="h-screen bg-[#424B54]">
       <App />
-    </>
+    </div>
   </React.StrictMode>
 );
