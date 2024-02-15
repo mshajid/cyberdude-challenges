cc20de7fe99f9f53e7d121933084091927d62117
Shajid <shajid1biz@gmail.com>
2024-02-15 15:39:05 +0530

    fix: button hover effect

--- javascript/03-react-js/07-my-bucket-lists/src/Components/Button.jsx
+++ javascript/03-react-js/07-my-bucket-lists/src/Components/Button.jsx
@@ -1,7 +1,7 @@
 const Button = ({ label }) => {
   return (
     <div>
-      <button className="bg-[#9B6A6C]/60 px-5 py-1.5 rounded-md my-1 text-white hover:bg-[#9B6A6C]/100">
+      <button className="bg-[#9B6A6C]/60 px-5 py-1.5 rounded-md my-1 text-white trnsition-all w-36 h-9 hover:border hover:border-gray-200 hover:bg-[#9B6A6C]/100">
         {label}
       </button>
     </div>
