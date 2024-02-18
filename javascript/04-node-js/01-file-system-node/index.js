9feda2eacc1cb43f7656ff134de545466fb8e5b2
Shajid <shajid1biz@gmail.com>
2024-02-19 02:16:08 +0530

    feat: added the file directory to the node

--- javascript/04-node-js/01-file-system-node/index.js
+++ javascript/04-node-js/01-file-system-node/index.js
@@ -1,9 +1,13 @@
 import * as fs from "fs";
 
+fs.mkdir('newFolder',(err) => {
+    err ? console.log(err) : console.log("folder created")
+})
+
 fs.writeFile(
   "HelloWorld.txt",
   `What's happening inside the Hello World text ${new Date()}`,
   (err) => {
-    err ? console.log(err) : console.log("File is updating")
+    err ? console.log(err) : console.log("File is updating");
   }
 );
