28920cae286e7682850b5ed5907572157eb73376
Shajid <shajid1biz@gmail.com>
2024-02-19 02:43:48 +0530

    feat: saved the filtered data on hello.txt

--- javascript/04-node-js/01-file-system-node/index.js
+++ javascript/04-node-js/01-file-system-node/index.js
@@ -1,26 +1,10 @@
 import * as fs from "fs";
-import internsData from "./data/interns.json" assert {type: 'json'}
+import internsData from "./data/interns.json" assert { type: "json" };
 
-console.log(internsData);
-
-// This is to create a directory or folder. 
-
-fs.mkdir("newFolder", (err) => {
-  err ? console.log(err) : console.log("folder created");
+const findIntern = internsData.find((value) => {
+  return value.id === 1;
 });
 
-// This is to write a file inside a directory
-
-fs.writeFile("newFolder/Hello.txt", "This is inside the folder", (err) => {
-  err ? console.log(err) : console.log("file is created");
-});
-
-
-// This is to write a file normally on the root itself
-fs.writeFile(
-  "HelloWorld.txt",
-  `What's happening inside the Hello World text ${new Date()}`,
-  (err) => {
-    err ? console.log(err) : console.log("File is updating");
-  }
-);
+fs.writeFile("HelloWorld.txt", JSON.stringify(findIntern), (err) => {
+    err ? console.log(err) : console.log("adding content")
+})
