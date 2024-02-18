e6005f9cc83bd1be815cd24c4df7767d8aceb164
Shajid <shajid1biz@gmail.com>
2024-02-19 02:19:21 +0530

    feat: created a directory and added a text on it

--- javascript/04-node-js/01-file-system-node/index.js
+++ javascript/04-node-js/01-file-system-node/index.js
@@ -1,9 +1,19 @@
 import * as fs from "fs";
 
-fs.mkdir('newFolder',(err) => {
-    err ? console.log(err) : console.log("folder created")
-})
+// This is to create a directory or folder. 
 
+fs.mkdir("newFolder", (err) => {
+  err ? console.log(err) : console.log("folder created");
+});
+
+// This is to write a file inside a directory
+
+fs.writeFile("newFolder/Hello.txt", "This is inside the folder", (err) => {
+  err ? console.log(err) : console.log("file is created");
+});
+
+
+// This is to write a file normally on the root itself
 fs.writeFile(
   "HelloWorld.txt",
   `What's happening inside the Hello World text ${new Date()}`,
