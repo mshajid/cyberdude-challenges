b1da372a3308056d3ece9ede28238466627f2415
Shajid <shajid1biz@gmail.com>
2024-02-20 00:35:40 +0530

    feat: added the api to the endpoint

--- javascript/04-node-js/02-react-api-endpoint/server/server.js
+++ javascript/04-node-js/02-react-api-endpoint/server/server.js
@@ -1,9 +1,11 @@
 const http = require("http");
+const internsData = require("./data/interns.json")
 
 const SERVER_PORT = 4455;
 
-http.createServer(function (request, response) {
-  console.log("server is running la");
-  response.write("Is server working? After fixing")
-  response.end()
-}).listen(SERVER_PORT)
+http
+  .createServer(function (request, response) {
+    response.write(JSON.stringify(internsData));
+    response.end();
+  })
+  .listen(SERVER_PORT);
