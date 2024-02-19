706409f6a409cbc55ee00a17e626dc8549cfb9fd
Shajid <shajid1biz@gmail.com>
2024-02-19 19:57:13 +0530

    prepping the task 1

--- javascript/04-node-js/01-file-system-node/index.js
+++ javascript/04-node-js/01-file-system-node/index.js
@@ -1,10 +1,5 @@
 import * as fs from "fs";
 import internsData from "./data/interns.json" assert { type: "json" };
 
-const findIntern = internsData.find((value) => {
-  return value.id === 1;
-});
+// Task 1
 
-fs.writeFile("HelloWorld.txt", JSON.stringify(findIntern), (err) => {
-    err ? console.log(err) : console.log("adding content")
-})
