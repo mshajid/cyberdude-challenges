eba0f791a065f0eabd1fb79cf8e76c84997efa1f
Shajid <shajid1biz@gmail.com>
2024-02-19 20:58:07 +0530

    feat: creating directories and list based on JSON

--- javascript/04-node-js/01-file-system-node/index.js
+++ javascript/04-node-js/01-file-system-node/index.js
@@ -1,5 +1,25 @@
 import * as fs from "fs";
-import internsData from "./data/interns.json" assert { type: "json" };
+import popPlaces from "./data/popularPlaces.json" assert { type: "json" };
 
 // Task 1
 
+fs.mkdir("Location", (err) => {
+  err && console.log(err);
+});
+
+popPlaces.map((place) => {
+  fs.mkdir(`Location/${place.city}`, (err) => {
+    err ? console.log(err) : "file is working";
+    place.places.map((city) => {
+      fs.writeFile(
+        `Location/${place.city}/${city.placeName}.txt`,
+        `${city.placeDesc}`,
+        (err) => {
+          err && console.log(err);
+        }
+      );
+    });
+  });
+});
+
+// Thanks to Yogha XD
