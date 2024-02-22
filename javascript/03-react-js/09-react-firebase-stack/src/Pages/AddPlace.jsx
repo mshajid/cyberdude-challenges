df3c79395ed8360dfad34aa27255d919e8456af7
Shajid <shajid1biz@gmail.com>
2024-02-23 01:25:17 +0530

    feat: added assets, made the image text

--- javascript/03-react-js/09-react-firebase-stack/src/Pages/AddPlace.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Pages/AddPlace.jsx
@@ -1,7 +1,15 @@
+import travelBg from "../assets/travel-form-bg.jpg";
+
 const AddPlace = () => {
   return (
     <>
-      <div>Add A Place Here</div>
+      <div className="relative flex justify-center items-center">
+        <img className="w-full h-56 object-cover" src={travelBg} />
+        <div className="text-6xl absolute font-semibold  text-white">
+          Add Your Favorite Place Here 🌴🌴
+        </div>
+      </div>
+      <div className="max-w-xl mx-auto"></div>
     </>
   );
 };
--- javascript/03-react-js/09-react-firebase-stack/src/Pages/Navbar.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Pages/Navbar.jsx
@@ -4,13 +4,20 @@ const Navbar = () => {
   return (
     <>
       <div className="bg-yellow-500 px-5 py-2 flex items-center justify-between">
-        <h2 className="text-3xl font-semibold tracking-wide">Hello Sri Lanka</h2>
+        <h2 className="text-3xl font-semibold tracking-wide">
+          Hello Sri Lanka
+        </h2>
         <div>
-            <Link className="bg-red-500 px-3 py-1 rounded-md text-white font-medium" to={"/add-place"}>Add Place Here</Link>
+          <Link
+            className="bg-red-500 px-3 py-1 rounded-md text-white font-medium"
+            to={"/add-place"}
+          >
+            Add Place Here
+          </Link>
         </div>
       </div>
     </>
   );
 };
 
-export default Navbar
+export default Navbar;
