f897be7825757d5295ede20f2bded0dd67f0660e
Shajid <shajid1biz@gmail.com>
2024-02-23 01:55:01 +0530

    feat: intergrated the hook form

--- javascript/03-react-js/09-react-firebase-stack/src/Components/FormInput.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Components/FormInput.jsx
@@ -1,8 +1,16 @@
-const FormInput = () => {
+const FormInput = ({ name, placeholder, label, register }) => {
   return (
-    <>
-      <div>FormInput</div>
-    </>
+    <div>
+      <label className="font-medium" htmlFor={name}>
+        {label}
+        <input
+          name={name}
+          placeholder={placeholder}
+          className="w-full px-2 outline-none text-sm py-1.5 bg-gray-100"
+          {...register}
+        />
+      </label>
+    </div>
   );
 };
 
--- javascript/03-react-js/09-react-firebase-stack/src/Components/MainForm.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Components/MainForm.jsx
@@ -1,11 +1,43 @@
+import { useForm } from "react-hook-form";
 import FormInput from "./FormInput";
+import FormButton from "./FormButton";
 
 const MainForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const formSubmit = (data) => {
+    return console.log(data)
+  } 
+
   return (
-    <>
+    <form onSubmit={handleSubmit(formSubmit)}>
       <h2>MainForm</h2>
-      <FormInput />
-    </>
+      <FormInput
+        label={"Traveler's Name Here"}
+        name={"travelerName"}
+        placeholder={"Enter traveler's full name here"}
+        register={register("travelerName")}
+      />
+      <FormInput
+        label={"Traveler's Name Here"}
+        name={"travelerName"}
+        placeholder={"Enter traveler's full name here"}
+        register={register("travelerName")}
+      />
+      <FormInput
+        label={"Traveler's Name Here"}
+        name={"travelerName"}
+        placeholder={"Enter traveler's full name here"}
+        register={register("travelerName")}
+      />
+      <FormInput
+        label={"Traveler's Name Here"}
+        name={"travelerName"}
+        placeholder={"Enter traveler's full name here"}
+        register={register("travelerName")}
+      />
+      <FormButton label={"Submit Your Place Here"} />
+    </form>
   );
 };
 
