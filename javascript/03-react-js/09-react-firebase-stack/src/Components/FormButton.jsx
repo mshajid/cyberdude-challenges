7f40c1cb73b1cb93b0529ed043fa2b84eacd5c37
Shajid <shajid1biz@gmail.com>
2024-02-23 01:54:16 +0530

    feat: added a formBtn component

---
+++ javascript/03-react-js/09-react-firebase-stack/src/Components/FormButton.jsx
@@ -0,0 +1,9 @@
+const FormButton = ({ label }) => {
+  return (
+    <>
+      <button className="bg-red-500 px-2 py-0.5 my-2 rounded-md">{label}</button>
+    </>
+  );
+};
+
+export default FormButton
