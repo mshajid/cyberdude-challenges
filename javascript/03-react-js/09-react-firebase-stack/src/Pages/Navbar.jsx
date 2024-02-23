109b8accbd02aa3401589c31cd996774d38b2f94
Shajid <shajid1biz@gmail.com>
2024-02-24 01:29:24 +0530

    fix: recreated the navbar

--- javascript/03-react-js/09-react-firebase-stack/src/Pages/Navbar.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Pages/Navbar.jsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
-      <div className="bg-yellow-500 px-5 py-2 flex items-center justify-between">
-        <h2 className="text-3xl font-semibold tracking-wide">
+      <div className="bg-[#001845] px-5 py-4 flex items-center justify-between">
+        <h2 className="text-3xl font-semibold tracking-wide text-white">
           Hello Sri Lanka
         </h2>
-        <div>
+        <div className="flex gap-x-5 items-center">
+          <Link className="text-[16px] text-white" to={"/"}>Home</Link>
+          <Link className="text-[16px] text-white" to={"/"}>About Us</Link>
+          <Link className="text-[16px] text-white" to={"/"}>Contact Us</Link>
           <Link
-            className="bg-red-500 px-3 py-1 rounded-md text-white font-medium"
+            className="bg-[#0466C8] px-3 py-1 rounded-md text-white font-medium"
             to={"/add-place"}
           >
             Add Place Here
