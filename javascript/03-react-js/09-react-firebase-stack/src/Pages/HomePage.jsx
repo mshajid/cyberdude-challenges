6e38ad9a54d206ba76dfb071bd9348da9042ebdb
Shajid <shajid1biz@gmail.com>
2024-02-23 12:16:42 +0530

    fix: homepage now receives the data from firebase

--- javascript/03-react-js/09-react-firebase-stack/src/Pages/HomePage.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Pages/HomePage.jsx
@@ -1,8 +1,28 @@
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../firebase";
+import { useEffect, useState } from "react";
+
 const HomePage = () => {
+  const [districts, setDistricts] = useState([]);
+
+  useEffect(() => {
+    async function getDataFromFirebase() {
+      const querySnapshot = await getDocs(collection(db, "badulla"));
+      const mappedData = querySnapshot.docs.map((doc) => doc.data());
+      setDistricts(mappedData)
+    }
+    getDataFromFirebase();
+  }, []);
+
   return (
-    <>
+    <div>
       <div>HomePage Is Here</div>
-    </>
+      {districts?.map((district) => {
+        return (
+          <h1>{district.districts}</h1>
+        )
+      })}
+    </div>
   );
 };
 
