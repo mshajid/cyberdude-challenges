1764c54a286eae58de81998eba42fccbf8a77061
Shajid <shajid1biz@gmail.com>
2024-02-24 02:27:23 +0530

    feat: added the HERO component

--- javascript/03-react-js/09-react-firebase-stack/src/Components/HeroBanner.jsx
+++ javascript/03-react-js/09-react-firebase-stack/src/Components/HeroBanner.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const HeroBanner = () => {
   return (
     <>
@@ -8,34 +10,39 @@ const HeroBanner = () => {
             src="https://images.pexels.com/photos/2937148/pexels-photo-2937148.jpeg?cs=srgb&dl=pexels-shaani-sewwandi-2937148.jpg&fm=jpg"
           />
 
+          <div className="absolute top-0 left-0 px-3 py-10 flex flex-col items-start h-96 gap-y-4">
+            <h2 className="text-4xl font-bold">Travel With Us <br/> Today</h2>
+            <span className="text-sm font-bold tracking-wide max-w-sm leading-5 text-[#001233] shadow-black drop-shadow-2xl">We've helped 1000+ tourists every summer to have their destination that breahtaking. Don't forget to help other tourists to add your favorite place in Sri Lanka.</span>
+          </div>
+
           <div className="flex absolute bottom-0 p-3 gap-x-3">
-            <div className="w-[320px] h-32 bg-red-500 rounded-md">
-              <div className="flex flex-col justify-center items-start h-32 p-2">
+            <Link to={"/add-place"} className="w-[320px] h-32 bg-[#002855]/80 hover:bg-[#002855] rounded-md transition-all px-1">
+              <div className="flex flex-col justify-center items-start h-32 p-4">
                 <h2 className="text-2xl text-white font-semibold">
                   Submit Your <br />
                   Favorite Place
                 </h2>
-                <span>Hassle free, Make your next move</span>
+                <em className="text-gray-200 text-sm">Hassle free, Make your next move</em>
               </div>
-            </div>
-            <div className="w-[320px] h-32 bg-red-500 rounded-md">
+            </Link>
+            <Link to={"/add-place"} className="w-[320px] h-32 bg-[#002855]/80 hover:bg-[#002855] rounded-md transition-all px-1">
               <div className="flex flex-col justify-center items-start h-32 p-2">
                 <h2 className="text-2xl text-white font-semibold">
-                  Submit Your <br />
-                  Favorite Place
+                  Make Your <br />
+                  Trip Remember
                 </h2>
-                <span>Hassle free, Make your next move</span>
+                <em className="text-gray-200 text-sm">Destination that equals to wonders</em>
               </div>
-            </div>
-            <div className="w-[320px] h-32 bg-red-500 rounded-md">
+            </Link>
+            <Link to={"/add-place"} className="w-[320px] h-32 bg-[#002855]/80 hover:bg-[#002855] rounded-md transition-all px-1">
               <div className="flex flex-col justify-center items-start h-32 p-2">
                 <h2 className="text-2xl text-white font-semibold">
-                  Submit Your <br />
-                  Favorite Place
+                  Budget Friendly <br />
+                  0% Expensive
                 </h2>
-                <span>Hassle free, Make your next move</span>
+                <em className="text-gray-200 text-sm">Unless, you move to expensive.</em>
               </div>
-            </div>
+            </Link>
           </div>
         </div>
       </div>
